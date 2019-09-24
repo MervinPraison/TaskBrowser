032855978d3ac53dc1443282bff68879b0ffa0a7
Mervin Praison <praison.ms@gmail.com>
2019-09-24 15:05:15 +0100

    Update tests.py

--- DjangoTaskBrowserDjango/tasks/browser/tests.py
+++ DjangoTaskBrowserDjango/tasks/browser/tests.py
@@ -10,8 +10,10 @@ class QuestionModelTests(TestCase):
 
     def test_was_scheduled_far_in_the_future(self):
         """
-        was_recently_started() returns False for tasks whose start_date
-        is far in the future.
+        was_recently_started() should provide the task which is recenlty started. So the given date/time should be in the past. 
+        This test is to find if the time provided is in the future. 
+        
+        was_recently_started() returns False for tasks whose start_date is in the future.
         """
         time = timezone.now() + datetime.timedelta(days=30)
         future_task = Task(start_date=time)
